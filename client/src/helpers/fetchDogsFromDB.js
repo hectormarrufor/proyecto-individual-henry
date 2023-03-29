3458c7d2cfacf4a111b52d1056726676d190beeb
Hector Marrufo <hectormmarrufor@gmail.com>
2023-03-29 00:01:24 -0400

    fixing export

--- client/src/helpers/fetchDogsFromDB.js
+++ client/src/helpers/fetchDogsFromDB.js
@@ -1,8 +1,9 @@
                 
-export const fetchDogsFromDB = async(dispatch, insert_replacing) => {
+const fetchDogsFromDB = async(dispatch, insert_replacing) => {
     let perrosDB = await fetch('http://localhost:3001/dogs');
     perrosDB = await perrosDB.json();
     dispatch(insert_replacing(perrosDB));
     
 }
 
+export default fetchDogsFromDB;
